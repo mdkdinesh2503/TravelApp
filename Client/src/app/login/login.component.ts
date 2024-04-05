a6576784c826975c9fb92e008825bb869faa49b4
mdkdinesh2503 <mdkdinesh2503@gmail.com>
2024-04-05 16:51:45 +0530

    Update Current Changes Occured

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,16 +1,78 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
+  providers:[UserService]
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private route: Router) {}
 
   ngOnInit() {}
+
+  condition: boolean = false;
+  displayStatus: boolean = false;
+  message:any;
+  imagePath:any;
+  color:any;
+
+  async submitList(usr: any, pss: any) {
+    await fetch('http://localhost:3000/registers', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          for (var i = 0; i < data.length; i++) {
+            if (((usr == data[i].USERNAME) || (usr == data[i].EMAIL)) && (pss == data[i].PASSWORD)) {
+              this.condition = true;
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+        if(this.condition) {
+          this.message = "Login Successfull";
+          this.imagePath = "../../assets/images/tick.png";
+          this.color = "green";
+          this.displayStatus = true;
+        } else {
+          this.message = "Inavlid Credentials";
+          this.imagePath = "../../assets/images/wrong.png";
+          this.color = "red";
+          this.displayStatus = true;
+        }
+  }
+
+  navigateActiveStatus() {
+    this.displayStatus = false;
+
+    if (this.imagePath == "../../assets/images/tick.png") {
+      this.route.navigate(['/register']);
+    }
+  }
+
+  fieldTextType: boolean = false;
+
+  showPass() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+
+  openForgotPass() {
+
+  }
 
 }
